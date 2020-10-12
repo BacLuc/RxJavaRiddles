8b35fd48f99108e5878a40846e6761c9b071fe2b
BacLuc <lucius.bachmann@gmx.ch>
2020-10-12 22:53:04 +0200

    Add file templates for Java Class and JUnit4 Testcase
    For the class to distinguish between java.util.observable and
    rx observable.
    The test template that the static imports for mockito etc are already there.

---
+++ .idea/fileTemplates/code/JUnit4 Test Class.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+#parse("File Header.java")
+
+public class ${NAME} {
+  ${BODY}
+}

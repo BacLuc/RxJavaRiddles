c53917a0f364ece04107564ec31450ecc2bcfbe0
Lucius Bachmann <lucius.bachmann@scs.ch>
2020-10-16 14:29:42 +0200

    RiddleCarouselTest: change to a more intuitive result
    Now that i know how to implement it.

--- src/test/java/com/vanniktech/rxriddles/operators/withscheduler/RiddleCarouselTest.java
+++ src/test/java/com/vanniktech/rxriddles/operators/withscheduler/RiddleCarouselTest.java
@@ -20,9 +20,9 @@ public class RiddleCarouselTest {
 	private static final String ACQUIRER_2 = "ACQUIRER_2";
 	private static final String ACQUIRER_3 = "ACQUIRER_3";
 	private static final List<String> ACQUIRERS_1 = Arrays.asList(ACQUIRER_1, ACQUIRER_2, ACQUIRER_3);
-	private static final String ACQUIRER_4 = "ACQUIRER_5";
-	private static final String ACQUIRER_5 = "ACQUIRER_6";
-	private static final String ACQUIRER_6 = "ACQUIRER_7";
+	private static final String ACQUIRER_4 = "ACQUIRER_4";
+	private static final String ACQUIRER_5 = "ACQUIRER_5";
+	private static final String ACQUIRER_6 = "ACQUIRER_6";
 	private static final List<String> ACQUIRERS_2 = Arrays.asList(ACQUIRER_4, ACQUIRER_5, ACQUIRER_6);
 
 	@Test
@@ -34,19 +34,19 @@ public class RiddleCarouselTest {
 		TestObserver<String> testObserver = RiddleCarousel.solve(acquirerCallable, FETCH_INTERVAL, CAROUSEL_INTERVAL, scheduler).test();
 
 		scheduler.advanceTimeBy(CAROUSEL_INTERVAL.toMillis() - 1, TimeUnit.MILLISECONDS);
-		testObserver.assertNoValues();
+		testObserver.assertValues(ACQUIRER_1);
 
 		scheduler.advanceTimeBy(1, TimeUnit.MILLISECONDS);
-		testObserver.assertValue(ACQUIRER_1);
+		testObserver.assertValues(ACQUIRER_1, ACQUIRER_2);
 
 		scheduler.advanceTimeBy(4 * CAROUSEL_INTERVAL.toMillis(), TimeUnit.MILLISECONDS);
-		testObserver.assertValues(ACQUIRER_1, ACQUIRER_2, ACQUIRER_3, ACQUIRER_1, ACQUIRER_2);
+		testObserver.assertValues(ACQUIRER_1, ACQUIRER_2, ACQUIRER_3, ACQUIRER_1, ACQUIRER_2, ACQUIRER_3);
 
 		Mockito.when(acquirerCallable.call()).thenReturn(ACQUIRERS_2);
-		scheduler.advanceTimeBy(2 * CAROUSEL_INTERVAL.toMillis(), TimeUnit.MILLISECONDS);
-		testObserver.assertValues(ACQUIRER_1, ACQUIRER_2, ACQUIRER_3, ACQUIRER_1, ACQUIRER_2, ACQUIRER_4);
+		scheduler.advanceTimeBy(CAROUSEL_INTERVAL.toMillis(), TimeUnit.MILLISECONDS);
+		testObserver.assertValues(ACQUIRER_1, ACQUIRER_2, ACQUIRER_3, ACQUIRER_1, ACQUIRER_2, ACQUIRER_3, ACQUIRER_4);
 
 		scheduler.advanceTimeBy(CAROUSEL_INTERVAL.toMillis(), TimeUnit.MILLISECONDS);
-		testObserver.assertValues(ACQUIRER_1, ACQUIRER_2, ACQUIRER_3, ACQUIRER_1, ACQUIRER_2, ACQUIRER_4, ACQUIRER_5);
+		testObserver.assertValues(ACQUIRER_1, ACQUIRER_2, ACQUIRER_3, ACQUIRER_1, ACQUIRER_2, ACQUIRER_3, ACQUIRER_4, ACQUIRER_5);
 	}
 }
